1891ddac9dc364b906c5cdd0f64518ddf4f1396a
Cameron Moy <mail@camoy.name>
2022-01-28 02:33:34 -0500

    Fix CI

--- info.rkt
+++ info.rkt
@@ -1,9 +1,20 @@
 #lang info
+
+;; General
+
 (define collection "meta")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/meta.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Associate metadata with values.")
 (define version "0.0")
 (define pkg-authors '(camoy))
+(define scribblings '(("scribblings/circuit.scrbl" ())))
 (define license '0BSD)
+
+;; Dependencies
+
+(define deps
+  '("base"))
+
+(define build-deps
+  '("chk-lib"
+    "racket-doc"
+    "scribble-lib"))
