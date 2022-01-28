f6b054381cb5289ade78a30127db976c640c136c
Cameron Moy <mail@camoy.name>
2022-01-28 02:34:39 -0500

    Typo

--- info.rkt
+++ info.rkt
@@ -6,7 +6,7 @@
 (define pkg-desc "Associate metadata with values.")
 (define version "0.0")
 (define pkg-authors '(camoy))
-(define scribblings '(("scribblings/circuit.scrbl" ())))
+(define scribblings '(("scribblings/meta.scrbl" ())))
 (define license '0BSD)
 
 ;; Dependencies
